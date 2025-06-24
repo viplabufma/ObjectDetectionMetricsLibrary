5fb763f55b5e0cc6d0f9a808b9bf1234b66cd237
Matheus Levy <matheus.levy@nca.ufma.br>
2025-06-23 23:23:24 -0300

    [UPDATE] Doc an Test CI

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="ObjectDetectionMetrics",
     version="0.1.0",
     author="Matheus Levy",
     author_email="matheus.levy@nca.ufma.br",
     description="Package for calculating object detection metrics",
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/seu-usuario/ObjectDetectionMetrics",
+    url="https://github.com/viplabufma/ObjectDetectionMetricsLibrary",
     packages=find_packages(),
     install_requires=[
         "numpy>=2.0.1",
@@ -18,10 +21,29 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Intended Audience :: Science/Research"
     ],
-    python_requires='>=3.12',
+    python_requires='>=3.8',
     keywords="object detection metrics coco precision recall map",
     license="MIT",
+    extras_require={
+        'test': [
+            'pytest>=8.2.2',
+            'pytest-cov>=4.0',
+            'coverage>=7.0'
+        ],
+        'dev': [
+            'twine>=5.0.0',
+            'build>=1.2.0',
+            'ipython>=8.0.0'
+        ]
+    }
 )
